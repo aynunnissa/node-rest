0d8a991f4bb326bee5a7b714d464ce92fe64dcb5
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-11-27 23:09:56 +0700

    feat: add checkout route

--- src/routes/cart-routes.ts
+++ src/routes/cart-routes.ts
@@ -1,31 +1,37 @@
 import { ServerResponse } from "http";
+import * as url from 'url';
 
 const cartController = require('../controllers/cartController');
 
 module.exports = (req: Request, res: ServerResponse) => {
-  switch (req.method) {
-    case 'GET':
-      cartController.getCartItems(req, res);
-      break;
-
-    case 'POST':
-      cartController.addItemToCart(req, res);
-      break;
-
-    case 'PUT':
-      cartController.updateCart(req, res);
-      break;
-
-    case 'DELETE':
-      cartController.removeItemFromCart(req, res);
-      break;
-
-    default:
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'application/json');
-      res.write(
-        JSON.stringify({ title: 'Not Found', message: 'Route not found' })
-      );
-      res.end();
+  const parsedUrl = url.parse(req.url, true);
+  if (parsedUrl.pathname === '/api/cart/checkout' && req.method === 'DELETE') {
+    cartController.checkoutCart(req, res);
+  } else {
+    switch (req.method) {
+      case 'GET':
+        cartController.getCartItems(req, res);
+        break;
+  
+      case 'POST':
+        cartController.addItemToCart(req, res);
+        break;
+  
+      case 'PUT':
+        cartController.updateCart(req, res);
+        break;
+  
+      case 'DELETE':
+        cartController.removeItemFromCart(req, res);
+        break;
+  
+      default:
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(
+          JSON.stringify({ title: 'Not Found', message: 'Route not found' })
+        );
+        res.end();
+    }
   }
 };
