a389e68c806f0382642dc54ff3381ac2b1dd0935
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-11-27 16:33:18 +0700

    feat: implement menu route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,14 @@ const url = require('url');
 
 // Routes
 const locationRoutes = require('./location-routes');
+const menuRoutes = require('./menu-routes');
 
 module.exports = (req: Request, res: ServerResponse) => {
   const parsedUrl = url.parse(req.url, true);
   if (parsedUrl.pathname.startsWith('/api/locations')) {
     locationRoutes(req, res);
+  } else if (parsedUrl.pathname.startsWith('/api/menus')) {
+    menuRoutes(req, res);
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'application/json');
