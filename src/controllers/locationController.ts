76b224e2d3324ba9b2e8df77c1d2ccd9939520a7
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-11-28 21:17:11 +0700

    feat: implement Location model

--- src/controllers/locationController.ts
+++ src/controllers/locationController.ts
@@ -1,19 +1,10 @@
 import { ServerResponse } from "http";
+import Location from "../db/models/location";
 
-const db = require('../db/index').db;
-
-const getLocations = (req: Request, res: ServerResponse) => {
-    const query = 'SELECT name, lat, lng AS `long` FROM locations';
-    db.query(query, (err: Error, results: Object) => {
-      if (err) {
-        console.error('MySQL query error:', err);
-        res.writeHead(500, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Internal Server Error' }));
-      } else {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(results));
-      }
-    });
+const getLocations = async (req: Request, res: ServerResponse) => {
+    const locations = await Location.findAll();
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ data: locations }));
 };
 
 module.exports = {
