4a25275886c18db91950bb34ab573ed23b384649
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-11-29 07:29:12 +0700

    fix: update routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,11 +10,12 @@ const cartRoutes = require('./cart-routes');
 
 module.exports = (req: Request, res: ServerResponse) => {
   const parsedUrl = url.parse(req.url, true);
-  if (parsedUrl.pathname.startsWith('/api/locations')) {
+  const path = parsedUrl.pathname?.split('/');
+  if (path[2] === 'locations') {
     locationRoutes(req, res);
-  } else if (parsedUrl.pathname.startsWith('/api/menus')) {
+  } else if (path[2] === 'menus') {
     menuRoutes(req, res);
-  } else if (parsedUrl.pathname.startsWith('/api/cart')) {
+  } else if (path[2] === 'cart') {
     cartRoutes(req, res);
   } else {
     notFoundResponse(res, 'Route not found')
